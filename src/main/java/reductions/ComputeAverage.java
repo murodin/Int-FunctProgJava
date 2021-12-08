20cd087f61a6b16bd690120a4f804b7c07bce4a1
simon <simon@dancingcloudservices.com>
2021-12-08 14:57:10 -0700

    Day 3, reduction lab solution (Averages)

--- src/main/java/reductions/ComputeAverage.java
+++ src/main/java/reductions/ComputeAverage.java
@@ -1,6 +1,10 @@
 package reductions;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 class Average {
   private double sum = 0;
@@ -11,7 +15,10 @@ class Average {
     this.count = count;
   }
 
-  public Average merge(Average other) {
+//  public static Set<String> threads =
+//      Collections.synchronizedSet(new HashSet<>());
+  public Average merge(Average this, Average other) {
+//    threads.add(Thread.currentThread().getName());
     return new Average(this.sum + other.sum, this.count + other.count);
   }
 
@@ -34,11 +41,29 @@ class Average {
 
 public class ComputeAverage {
   public static void main(String[] args) {
-    // create a stream of random numbers
+    // create a stream of random numbers as *double*
+    // Look at the Random class :)
     //  - in the range plus and minus X
     //  -- therefore the average should be about zero :)
     // map them to Averages of one value
     // reduce them to a single Average object
     // extract the result and print it
+
+    long start = System.nanoTime();
+    ThreadLocalRandom.current().doubles(4_000_000_000L, -Math.PI, +Math.PI)
+        .parallel()
+        .mapToObj(d -> new Average(d, 1))
+        .reduce(new Average(0, 0), (a1, a2) -> a1.merge(a2))
+        .get()
+        .map(d -> "The average is " + d)
+        .ifPresent(System.out::println);
+
+//    System.out.println("There were " + Average.threads.size() + " threads");
+//    for (String t : Average.threads) {
+//      System.out.println(t);
+//    }
+
+    long time = System.nanoTime() - start;
+    System.out.println("Time take is " + (time / 1_000_000_000.0) + " seconds");
   }
 }
